d9cd727430be56bdda395d8028d317daf21a9e50
Fidel <fgonza13@asu.edu>
2016-02-16 11:12:54 -0700

    serving size
    recommended serving size and nav from food groups

---
+++ T-REX/Back.swift
@@ -0,0 +1,13 @@
+//
+//  Back.swift
+//  T-REX
+//
+//  Created by Fidel Gonzalez on 2/16/16.
+//  Copyright © 2016 Delta Tech. All rights reserved.
+//
+
+import Cocoa
+
+class Back: NSObject {
+
+}
