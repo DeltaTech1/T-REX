9f7c2825c8fa9908681a544e49d9f10709b65212
DeltaTech1 <Dyan@Dyans-Mac.local>
2016-03-22 14:51:26 -0700

    3/22
    - Color theme
    - Cheese image
    - Progress bars on Choose Food Group
    - Trophies page

---
+++ T-REX/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  T-REX
+//
+//  Created by Dyan Abad on 3/21/16.
+//  Copyright © 2016 Delta Tech. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+}
---
+++ T-REX/TrophiesViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TrophiesViewController.swift
+//  T-REX
+//
+//  Created by Dyan Abad on 3/21/16.
+//  Copyright © 2016 Delta Tech. All rights reserved.
+//
+/*
+import UIKit
+
+class TrophiesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    
+    
+    let trophies = ["5-A-Day", "Strong Bones"]
+    let imageArray = [UIImage(named: "trophy600px"), UIImage(named: "trophy600px")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+*/
