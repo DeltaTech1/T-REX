7d9ff11a2b69b08a5c5b040926d03bde794c6bab
DeltaTech1 <Dyan@Dyans-Mac.local>
2016-03-24 12:24:13 -0700

    Fixed Trophies + Requirements code

--- T-REX/TrophiesViewController.swift
+++ T-REX/TrophiesViewController.swift
@@ -5,15 +5,17 @@
 //  Created by Dyan Abad on 3/21/16.
 //  Copyright © 2016 Delta Tech. All rights reserved.
 //
-/*
+
 import UIKit
 
 class TrophiesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     
     
     let trophies = ["5-A-Day", "Strong Bones"]
     let imageArray = [UIImage(named: "trophy600px"), UIImage(named: "trophy600px")]
+    let reqImages = [UIImage(named: "apple600"), UIImage(named: "milkbox600px")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +27,38 @@ class TrophiesViewController: UIViewController, UICollectionViewDelegate, UIColl
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.trophies.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! CollectionViewCell
+        
+        cell.imageView?.image = self.imageArray[indexPath.row]
+        cell.titleLabel?.text = self.trophies[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        self.performSegueWithIdentifier("showRequirements", sender: self)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showRequirements"
+        {
+            let indexPaths = self.collectionView!.indexPathsForSelectedItems()!
+            let indexPath = indexPaths[0] as NSIndexPath
+            
+            let viewController = segue.destinationViewController as! RequirementsViewController
+            viewController.image = self.reqImages[indexPath.row]!
+            viewController.title = self.trophies[indexPath.row]
+            
+        }
+    }
+    
+    
     
 
     /*
@@ -38,4 +72,3 @@ class TrophiesViewController: UIViewController, UICollectionViewDelegate, UIColl
     */
 
 }
-*/
